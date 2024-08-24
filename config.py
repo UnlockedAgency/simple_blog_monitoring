2c53758f5e378398ef4625aa08df5ec03d4d232e
UnlockedAgency <unlockedagency@gmail.com>
2024-08-24 11:18:37 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+SMTP_USERNAME = 'your_sender_email'
+SMTP_PASSWORD = 'xxxxxxxxxxxxxxxx'  # Create a Gmail app password(must have 2fa enabled)
+
+EMAIL_FROM = 'your_sender_email'
+EMAIL_TO = 'recipient_email'
